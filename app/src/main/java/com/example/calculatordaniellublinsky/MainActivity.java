1809b194176a1a0fc129d4b6ca992434a7349507
Daniel <daniel.lub2007@gmail.com>
2024-04-06 18:26:30 +0300

    1

---
+++ app/src/main/java/com/example/calculatordaniellublinsky/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.calculatordaniellublinsky;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.EditText;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity
+{
+    EditText input;
+    Button plus;
+    Button minus;
+    Button multiply;
+    Button divide;
+    Button equals;
+    Button AC;
+    Button credits;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        input = findViewById(R.id.input);
+        plus = findViewById(R.id.plus);
+        minus = findViewById(R.id.minus);
+        multiply = findViewById(R.id.multiply);
+        divide = findViewById(R.id.divide);
+        equals = findViewById(R.id.equals);
+        AC = findViewById(R.id.AC);
+        credits = findViewById(R.id.credits);
+
+    }
+
+
+}
